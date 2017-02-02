202748c76c53906dd309ea3acad26755dd950aff
Magdalena <magdalenaconevska.10@gmail.com>
2017-02-02 22:31:55 +0100

    History & Daily initial design

---
+++ NLife/app/src/main/java/com/example/magdalena/nlife/HistoryActivity.java
@@ -0,0 +1,36 @@
+package com.example.magdalena.nlife;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerDays);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.days_array, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+    }
+
+    public void startDailyIntakeActivity(View v){
+
+        Intent intent = new Intent(this, DailyIntakeActivity.class);
+        startActivity(intent);
+
+    }
+
+}
--- NLife/app/src/main/java/com/example/magdalena/nlife/HomeActivity.java
+++ NLife/app/src/main/java/com/example/magdalena/nlife/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.magdalena.nlife;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void startHistoryActivity(View v){
+
+       Intent intent = new Intent(this,HistoryActivity.class);
+        startActivity(intent);
+
+
+
     }
 }
