ca69f334f81c39249b48b7dcc6c51a64cb88f4cf
Marija <mtodosovska@gmail.com>
2017-02-03 21:07:33 +0100

    Changed SearchActivity form a basic activity to an empty activity.

--- NLife/app/src/main/java/com/example/magdalena/nlife/SearchActivity.java
+++ NLife/app/src/main/java/com/example/magdalena/nlife/SearchActivity.java
@@ -1,11 +1,7 @@
 package com.example.magdalena.nlife;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 public class SearchActivity extends MasterActivity {
 
@@ -13,18 +9,5 @@ public class SearchActivity extends MasterActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 }
