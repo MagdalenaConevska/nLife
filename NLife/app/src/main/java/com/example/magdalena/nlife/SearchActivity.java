9f1f4c5feb8ede644f2ea3815f9a86071303a435
Marija <mtodosovska@gmail.com>
2017-02-04 17:42:53 +0100

    Added the option of choosing a category in SearchActivity.

--- NLife/app/src/main/java/com/example/magdalena/nlife/SearchActivity.java
+++ NLife/app/src/main/java/com/example/magdalena/nlife/SearchActivity.java
@@ -2,6 +2,10 @@ package com.example.magdalena.nlife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class SearchActivity extends MasterActivity {
 
@@ -9,5 +13,11 @@ public class SearchActivity extends MasterActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinEnterCategory);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
     }
 }
