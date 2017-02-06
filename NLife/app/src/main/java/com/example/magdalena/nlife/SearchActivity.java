e7d56a7cad95a5202bc6b74726f60e00dc51267b
Marija <mtodosovska@gmail.com>
2017-02-06 18:03:26 +0100

    Added fragment transaction for the ProductDetailFragment.

--- NLife/app/src/main/java/com/example/magdalena/nlife/SearchActivity.java
+++ NLife/app/src/main/java/com/example/magdalena/nlife/SearchActivity.java
@@ -1,16 +1,15 @@
 package com.example.magdalena.nlife;
 
-import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.TextView;
+
 
 public class SearchActivity extends MasterActivity {
 
@@ -31,6 +30,23 @@ public class SearchActivity extends MasterActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+
+        ListView lv = (ListView)findViewById(R.id.lvSearch);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //make fragment transaction
+
+                ProductDetailFragment fragment = new ProductDetailFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+               // fragmentTransaction.replace(R.id.activity_search, fragment);
+                fragmentTransaction.commit();
+                //inside the new fragment construct the design according to the choice
+            }
+        });
+
 
     }
 }
