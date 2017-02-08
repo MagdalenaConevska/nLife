79b03695436cb9a66e77f5f94d438cd1c8c597ec
Magdalena <magdalenaconevska.10@gmail.com>
2017-02-08 23:16:03 +0100

    Half tuple

---
+++ NLife/app/src/main/java/com/example/magdalena/nlife/Tuple.java
@@ -0,0 +1,32 @@
+package com.example.magdalena.nlife;
+
+/**
+ * Created by Magdalena on 2/8/2017.
+ */
+
+public class Tuple {
+
+    public  String name = "nLife_nutrients";
+    public  String date = "date";
+    public  String id = "ndbno";
+    public  String quantity = "quantity";
+    public  String protein = "location";
+    public  String lipid = "genre";
+    public  String COLUMN_CARBOHYDRATE= "year";
+    public  String COLUMN_GLUCOSE = "dateRecorded";
+    public  String COLUMN_CALCIUM = "dateRecorded";
+    public  String COLUMN_IRON = "dateRecorded";
+    public  String COLUMN_MAGNESIUM = "dateRecorded";
+    public  String COLUMN_ZINC = "dateRecorded";
+    public  String COLUMN_VITAMIN_C= "dateRecorded";
+    public  String COLUMN_THIAMIN = "dateRecorded";
+    public  String COLUMN_RIBOFAVIN = "dateRecorded";
+    public  String COLUMN_NIACIN = "dateRecorded";
+    public  String COLUMN_VITAMIN_B6 = "dateRecorded";
+    public  String COLUMN_VITAMIN_B12 = "dateRecorded";
+    public  String COLUMN_VITAMIN_A = "dateRecorded";
+    public  String COLUMN_VITAMIN_D = "dateRecorded";
+    public  String COLUMN_VITAMIN_E = "dateRecorded";
+
+
+}
