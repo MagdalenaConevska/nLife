7252ac2c35b9c817c6dbdec73c1874fbc6c0479c
Martina <martina.tosevska.95@gmail.com>
2017-02-09 13:49:11 +0100

    Changes

--- NLife/app/src/main/java/com/example/magdalena/nlife/Nutrient.java
+++ NLife/app/src/main/java/com/example/magdalena/nlife/Nutrient.java
@@ -1,10 +1,13 @@
 package com.example.magdalena.nlife;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Marija on 2/6/2017.
  */
 
-public class Nutrient {
+public class Nutrient implements Parcelable{
     String name;
     Double recorded;
     String unit;
@@ -16,5 +19,35 @@ public class Nutrient {
         this.name = name;
         this.recorded = recorded;
         this.unit = unit;
+    }
+
+    protected Nutrient(Parcel in) {
+        name = in.readString();
+        unit = in.readString();
+        recorded=in.readDouble();
+    }
+
+    public static final Creator<Nutrient> CREATOR = new Creator<Nutrient>() {
+        @Override
+        public Nutrient createFromParcel(Parcel in) {
+            return new Nutrient(in);
+        }
+
+        @Override
+        public Nutrient[] newArray(int size) {
+            return new Nutrient[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(unit);
+        dest.writeDouble(recorded);
     }
 }
